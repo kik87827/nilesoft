2e5968865576c65b4af08dbc15c523bff06aeca0
kik8727 <kik87827@gmail.com>
2024-04-10 22:18:53 +0900

    레이아웃 작업중

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -1,6 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
     commonInit();
-    selectFunc();
 });
 window.addEventListener("load", () => {
   layoutFunc();
@@ -58,7 +57,52 @@ $(function() {
    * 레이아웃
    */
   function layoutFunc() {
+    function scrollTop(){
+      const btn_top_go = document.querySelector(".btn_top_go");
+      btn_top_go.addEventListener("click",(e)=>{
+        e.preventDefault();
+        window.scrollTo({
+          top : 0,
+          left : 0,
+          behavior : "smooth"
+        })
+      });
+    }
+    function pcGnb(){
+      const header_wrap = document.querySelector(".header_wrap");
+      const bg_depth = document.querySelector(".bg_depth");
+      const header_nav_list = document.querySelector(".header_nav_list");
+      const header_nav_li = document.querySelectorAll(".header_nav_list > li");
+      if(!!header_nav_li){
+        header_nav_li.forEach((this_li)=>{
+          this_li.addEventListener("mouseenter",(e)=>{
+            const this_item = e.currentTarget;
+            const this_item_not = siblings(this_item);
+            const this_item_depth = this_item.querySelector(".header_two_list_wrap");
 
+            this_item_not.forEach((item)=>{
+              item.classList.remove("active");
+            });
+    
+            this_item.classList.add("active");
+            header_wrap.classList.add("active");
+    
+            bg_depth.style.height = this_item_depth.getBoundingClientRect().height + "px";
+          });
+        });
+      }
+      if(!!header_wrap){
+        header_wrap.addEventListener("mouseleave",()=>{
+          header_nav_li.forEach((item)=>{
+            header_wrap.classList.remove("active");
+            item.classList.remove("active");
+          });
+          bg_depth.style.height = "0px";
+        });
+      }
+    }
+    pcGnb();
+    scrollTop();
   }
   
   /**
